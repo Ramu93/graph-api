0fdb637bc85a6a351962d799a145e4e081652332
Ramu Ramasamy <r.ramasamy@livello.com>
2020-10-10 20:12:22 +0200

    save

--- src/instagram/instagram.controller.ts
+++ src/instagram/instagram.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Logger, Post } from '@nestjs/common';
 import { ProfileDto } from './dtos/profile.dto';
-import { Profile } from './interfaces/profile.interface';
 import { ProfileRepository } from './repositories/profile.repository';
 
 const Instagram = require('instagram-web-api');
@@ -9,18 +8,30 @@ const Instagram = require('instagram-web-api');
 export class InstagramController {
   constructor(readonly profileRepository: ProfileRepository) {}
 
-  @Get()
-  async login() {
-    const instagramClient = new Instagram({
-      username: process.env.INSTAGRAM_USERNAME,
-      password: process.env.INSTAGRAM_PASSWORD,
-    });
-
-    await instagramClient.login();
-    const profile = new ProfileDto(await instagramClient.getProfile());
-
-    await this.profileRepository.save(profile);
-    // const movies = this.movieRepository.findAll();
-    return profile;
+  @Get('/getData')
+  async getDataFromInstagram(): Promise<{message: string}> {
+    try {
+      const instagramClient = new Instagram({
+        username: process.env.INSTAGRAM_USERNAME,
+        password: process.env.INSTAGRAM_PASSWORD,
+      });
+  
+      //login
+      await instagramClient.login();
+  
+      // get profile details and save to database
+      const profile = new ProfileDto(await instagramClient.getProfile());
+      await this.profileRepository.save(profile);
+  
+      // logout
+      // await instagramClient.logout();
+      // const movies = this.movieRepository.findAll();
+      
+      Logger.log('🔔 [SUCCESS] Data fetched from Instagram API and stored.');
+      return {message: 'success'};
+    } catch (error) {
+      Logger.error('🔔 [FAILURE] Data fetch and store failed.', error);
+      return {message: 'failure'};
+    }
   }
 }
